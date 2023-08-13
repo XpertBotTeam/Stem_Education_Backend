93daa0e8e76d6e47ba2c180fa819a2a93ef23e5b
= <=>
2023-08-13 20:24:24 +0300

    creating database migrations and model relationship

--- database/migrations/2023_08_13_155221_create_courses_table.php
+++ database/migrations/2023_08_13_155221_create_courses_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->number('phone_number');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('photo')->nullable();
+            $table->text('biography');
+            $table->string('link');
+            $table->date('release_date')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 };
--- database/migrations/2023_08_13_164826_create_category_user_table.php
+++ database/migrations/2023_08_13_164826_create_category_user_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category_user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->number('phone_number');
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('category_id')->constraint()->onDelete('cascade');
+            $table->foreignId('user_id')->constraint()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category_user');
     }
 };
